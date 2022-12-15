31e7f80bcd32b971eea79d1398871821c280868b
Tyler King <doitliketyler@gmail.com>
2022-12-15 13:29:30 -0800

    Update api request

--- lib/apiService.ts
+++ lib/apiService.ts
@@ -1,9 +1,10 @@
 class apiService {
   static async GetCreatures() {
-    const res = await fetch('./api/creatures', {
+    const res = await fetch('https://bestiary-next.netlify.app/api/creatures', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
+        'access-control-allow-orgigin': '*'
       }
     })
     return await res.json()
