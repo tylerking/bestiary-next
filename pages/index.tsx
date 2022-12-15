2b8283105a72dba66669cfc81baaf9c60b588131
Tyler King <doitliketyler@gmail.com>
2022-12-15 12:49:58 -0800

    Update template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,17 @@
 import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
 import styles from '../styles/Home.module.css'
-//import Creatures from '../components/Creatures'
+import Creatures from '../components/Creatures'
 
-export async function getServerSideProps() {
-  try {
-    const client = await clientPromise
-    const db = client.db('bestiary')
-
-    const creatures = await db
-      .collection('creatures')
-      .find({})
-      .sort({ name: 1 })
-      .limit(50)
-      .toArray()
-
-    return {
-      props: { creatures: JSON.parse(JSON.stringify(creatures)) },
-    }
-  } catch (e) {
-    console.error(e)
-  }
-}
-
-const Home = ({ creatures }: any) => {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>American Bestiary</title>
-        <meta
-          name='description'
-          content='Famous Mythical Creatures of the United States'
-        />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Famous Mythical Creatures of the United States" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <ul>
-        {creatures.map((creature: any, index: number) => (
-          <li key={index}>
-            <h2>{creature.name}</h2>
-          </li>
-        ))}
-      </ul>
+      <Creatures/>
     </div>
   )
 }
-
-export default Home
